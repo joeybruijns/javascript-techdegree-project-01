252769a8f33d93117289fb840eec4f65825c5c6f
Joey Bruijns <39805974+joeybruijns@users.noreply.github.com>
2020-04-24 23:00:11 +0200

    Code refactor for script.js

--- js/script.js
+++ js/script.js
@@ -37,9 +37,6 @@ let quotes = [
     }
 ];
 
-// A list of different color values
-let bgColors = ["#6eb362", "#b2088a", "#e483dc", "#4C9BB2"];
-
 // A function for picking a random quote from the array
 function getRandomQuote() {
     let randomNumber = Math.floor(Math.random() * quotes.length);
@@ -48,51 +45,39 @@ function getRandomQuote() {
 
 // A function that changes the background color of the page and the button
 function changeBackground() {
-    let randomColor = Math.floor(Math.random() * bgColors.length);
-    document.body.style.backgroundColor = bgColors[randomColor];
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let color = `rgb(${x} , ${y} , ${z})`;
 
-    let button = document.getElementById('loadQuote');
-    button.style.backgroundColor = bgColors[randomColor];
-
-    button.onmouseover = hoverOver;
-    button.onmouseleave = buttonColor;
-
-    function buttonColor() {
-        let button = document.getElementById('loadQuote');
-        button.style.backgroundColor = bgColors[randomColor];
-    }
-
-    function hoverOver() {
-        let elem = document.getElementById('loadQuote');
-        elem.style.backgroundColor = "rgba(255,255,255,.25)";
-    }
+    document.body.style.backgroundColor = color;
+    document.getElementById('loadQuote').style.backgroundColor = color;;
 }
 
 // A function for printing the random quote to the screen
 function printQuote() {
     let randomQuote = getRandomQuote();
-    let htmlString = "<p class=\"quote\">" + randomQuote.quote + "</p>";
-    htmlString += "<p class=\"source\">" + randomQuote.source;
+    let htmlString = `<p class="quote">${randomQuote.quote}</p>`;
+    htmlString += `<p class="source">${randomQuote.source}`;
 
     if (randomQuote.citation) {
-        htmlString += "<span class=\"citation\">" + randomQuote.citation + "</span>";
+        htmlString += `<span class="citation">${randomQuote.citation}</span>`;
+    }
+    if (randomQuote.year) {
+        htmlString += `<span class="year">${randomQuote.year}</span>`;
+    }
+    if (randomQuote.tag) {
+        htmlString += `<span class="tag">${randomQuote.tag}</span>`;
     }
 
-    if (randomQuote.year) {
-        htmlString += "<span class=\"year\">" + randomQuote.year + "</span>";
-    }
-    htmlString += "<span class=\"tag\">" + randomQuote.tag + "</span></p>";
+    htmlString += `</p>`;
 
     document.getElementById('quote-box').innerHTML = htmlString;
-    changeBackground()
+    changeBackground();
 }
 
-// interval function for changing quotes automatically
-function interval() {
-    setInterval(printQuote, 25000);
-}
-
-interval();
+// call the setInterval function for changing quotes automatically
+const interval = setInterval(printQuote, 25000);
 
 // When the button is clicked, a random quote is displayed and the background color is changed
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
